3af5ed30b83a137ace6b371eefcb2b2b2a352807
pakd <deepak.kumar.cse12@gmail.com>
2019-05-04 23:09:02 +0530

    elevator system design further added

--- OOPStandardProblems/ElevatorSystemDesign.cpp
+++ OOPStandardProblems/ElevatorSystemDesign.cpp
@@ -21,11 +21,70 @@ enum Door
     CLOSE
 };
 
-struct Request
+class Request
 {
-    long time;
-    int floor;
-    Direction direction;
+public:
+    Request(int floor, Direction direction)
+    {
+        mTime = 1540; // system timestamp
+        mFloor = floor;
+        mDirection = direction;
+    }
+
+
+    int getTime()
+    {
+        return mTime;
+    }
+
+    int getFloor()
+    {
+        return mFloor;
+    }
+
+    Direction getDirection()
+    {
+        return mDirection;
+    }
+
+private:
+
+    long mTime;
+    int mFloor;
+    Direction mDirection;
+};
+
+class FloorRequest : public Request
+{
+public:
+    FloorRequest(int srcFloor, Direction direction) : Request(srcFloor, direction) // random timestamp
+    {
+
+    }
+private:
+
+};
+
+class ElevatorRequest : public Request
+{
+public:
+    ElevatorRequest(int targetFloor, Direction direction) : Request(targetFloor, direction) // random timestamp
+    {
+
+    }
+private:
+
+};
+
+// class for priority queue
+class ComparisonClass
+{
+public:
+    bool operator() (FloorRequest a, FloorRequest b)
+    {
+        //comparison code here: return max in case of min heap
+        return a.getTime() > b.getTime();
+    }
 };
 
 class Elevator
@@ -56,6 +115,11 @@ public:
     }
     // other setters and getters
 
+    Direction getDirection()
+    {
+        return mDirection;
+    }
+
     void gotoFloor(int floor)
     {
         // direction can be decided by mCurrentFloorLocation and floor passed here
@@ -70,9 +134,51 @@ public:
         setDoor(Door::CLOSE);
     }
 
-    void putRequest(int floor, Direction direction)
+    void process()
+    {
+        // pick the earliest time stamp request from queues and handle them;
+        // FloorRequestObj
+        // goToFloor(FloorRequestObj.getFloor());
+        // handleElevatorRequest(targetFloor, direction)
+        // change queue after sometime or after completing trips
+        // after going to target floor make it false
+    }
+
+    void handleElevatorRequest(int targetFloor, Direction direction)
+    {
+        ElevatorRequest ElevatorRequestObj(targetFloor, direction);
+        mTargetFloorArr[targetFloor] = true;
+    }
+
+    void handleFloorRequest(int srcFloor, Direction direction)
     {
 
+        FloorRequest FloorRequestObj(srcFloor, direction);
+
+        // case 1: if direction is up and lift is also going up
+        if(getDirection() == Direction::UP)
+        {
+            if(srcFloor >= mCurrentFloorLocation)
+            {
+                currentQueue.push(FloorRequestObj);
+            }
+            else
+            {
+                upQueue.push(FloorRequestObj);
+            }
+        }
+
+        else // (direction == Direction::DOWN)
+        {
+            if(srcFloor <= mCurrentFloorLocation)
+            {
+                currentQueue.push(FloorRequestObj);
+            }
+            else
+            {
+                downQueue.push(FloorRequestObj);
+            }
+        }
     }
 
 private:
@@ -85,26 +191,35 @@ private:
     int mCurrentFloorLocation;
     bool *mFloorArr; // supported floors;
     bool *mTargetFloorArr; // target floors
+    int capacity; // to check overloaded or not
 
-    queue<Request> currentQueue;
-    queue<Request> upQueue;
-    queue<Request> downQueue;
+    priority_queue<FloorRequest, vector<FloorRequest>, ComparisonClass > currentQueue;
+    priority_queue<FloorRequest, vector<FloorRequest>, ComparisonClass > upQueue;
+    priority_queue<FloorRequest, vector<FloorRequest>, ComparisonClass > downQueue;
 
 };
 
 class ElevatorSystem
 {
-    // one elevator can have multiple elevators
+    // one ElevatorSystem can have multiple elevators
     // job of elevator system is to optimize the elevator's logic
     // and issue requests to different elevators
 public:
+    ElevatorSystem()
+    {
+
+    }
+    void addElevator(Elevator elevator)
+    {
+        mElevatorList.push_back(elevator);
+    }
 private:
     list<Elevator> mElevatorList;
 };
 
 
-
 int main()
 {
-
+    ElevatorSystem ElevatorSystemObj;
+    return 0;
 }
